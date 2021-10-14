f6efce44c7dde53e5270f3c3674559885a777140
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2021-10-14 09:31:04 -0400

    created the board
    going to make this its own class so it takes up less space in the code and we can just call it

---
+++ Battleboard.java
@@ -0,0 +1,84 @@
+public class Battleboard{
+	
+	
+	
+	public static void main (String[] args) {
+	
+	int [][] board = new int[10][10];//makes array
+	
+	//we should make this a board class to print this in one and save space
+	System.out.println("YOUR SHIPS");
+	System.out.println("  |1|2|3|4|5|6|7|8|9|10| ");
+	System.out.println("A |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("B |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("C |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("D |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("E |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("F |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("G |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("H |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("I |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("J |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	
+	System.out.println("OPPONENT SHIPS");
+	System.out.println("  |1|2|3|4|5|6|7|8|9|10| ");
+	
+	System.out.println("A |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("B |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("C |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("D |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("E |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("F |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("G |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("H |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("I |_|_|_|_|_|_|_|_|_|_| ");
+	
+	System.out.println("J |_|_|_|_|_|_|_|_|_|_| ");
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
