b4410ccf0ba3b5b52804b54efb81eb423172948e
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2021-10-17 21:22:14 -0400

    added sudo code

--- Ship.java
+++ Ship.java
@@ -1,5 +1,13 @@
 //handles each ship object
 
+//SUDO CODE/PLAN FOR THIS CLASS:
+//So from the Board class, 3 ship instances get called with 3, 4, and 5 as the lengths. Each ship has their own
+//corresponding letter so ship3 will be represented with A's, ship4 with B's, etc. And if the marker is in the same place
+//as one of the ships in the 2d array, then something like ship3.hit() will be called. That function will reduce the lives
+//of said ship as it's character (a, b or c) will turn into a 0 on the board. Once the ship runs out of lives, it is dead so
+//the function just returns true for dead (that also means when the function ship.hit() is called, it will need to be
+//stored in a boolean variable).
+
 public class Ship {
     int lives;
 
