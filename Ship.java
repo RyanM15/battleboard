486eff94034f99d061723bab95e7b4e14b42a663
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2021-10-17 21:08:49 -0400

    added comments

--- Ship.java
+++ Ship.java
@@ -1,11 +1,15 @@
+//handles each ship object
 import java.util.Scanner;
 
 public class Ship {
     int lives;
+
+    //passes through length of the ship, which is essentially how many lives it has
     public Ship (length){
-        int lives = length;
+        lives = length;
     }
 
+    //gets called from another class and reduces the lives. Also returns true if it is dead or destroyed
     public boolean hit(){
         lives--;
         if(lives <= 0){
