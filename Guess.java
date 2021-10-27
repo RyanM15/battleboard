2a241e2fca0a5b748d3ae01e77e9088ed6a4f74c
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2021-10-27 19:52:23 -0400

    added the basis for marker functions

--- Guess.java
+++ Guess.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 
 public class Guess {
 	System.out.println("Where do you guess?");
+	if (playerGuess = computerShip1 || playerGuess = computerShip2 || playerGuess = computerShip3)
+	{
+		battleboard.hitmarker();
+	}
+	else 
+	{
+		battleboard.missmarker();
+	}
+}
 	
-
-
-
-
+	
 
 
 
