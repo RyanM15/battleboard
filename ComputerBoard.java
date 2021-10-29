0dd69cb99c06efb10a8bb770747fcccf58c7e4db
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2021-10-29 08:06:48 -0400

    added random ship placement

--- ComputerBoard.java
+++ ComputerBoard.java
@@ -1,36 +1,129 @@
-public int playerGuess(int row, int col){
-	board[3][4] = 'a';
-	
-	
-	int totalShips = 1;
-	
-	
-	
-	
-	if (board [row][col] == 'a')
+public class ComputerBoard{
+    char [][] board = new char[10][10];
+    int rowPos = 0;
+    int colPos = 0;
+    boolean placed = false;
+    double orientation = 0;
+    double direction = 0;
+
+    public void placeShips(){
+        for (int r = 0; r < 10; r++)//rows
+	    {
+	    	for (int c = 0; c < 10; c++)//columns
+	    	{
+	    	    board[r][c] = ' ';
+		    }
+	    }
+
+        placed = false;
+        while (placed == false){
+            boolean p = ship(3, 'a');
+            if(p){
+                placed = true;
+            }
+        }
+        placed = false;
+        while (placed == false){
+            boolean p = ship(4, 'b');
+            if(p){
+                placed = true;
+            }
+        }
+        placed = false;
+        while (placed == false){
+            boolean p = ship(5, 'c');
+            if(p){
+                placed = true;
+            }
+        }
+        
+       
+    }
+    public boolean ship(int length, char letter){
+        for(int i = 0; i<length; i++){
+                if(i==0){
+                    rowPos = ((int) (Math.random() * (9 - 0)) + 0);
+                    colPos = ((int) (Math.random() * (9 - 0)) + 0);
+                    orientation = Math.random();
+                    direction = Math.random();
+                    if(orientation > .5){
+                        if(colPos + (length-1) > 9){
+                            direction=-1;
+                        }
+                        if(colPos - (length-1) < 0){
+                            direction=1;
+                        }
+                    }else{
+                         if(rowPos + (length-1) > 9){
+                            direction=-1;
+                        }
+                        if(rowPos - (length-1) < 0){
+                            direction=1;
+                        }
+                    }
+                }else{
+                    if(orientation > .5){//change columns
+                        if(direction > .5){//add
+                            colPos++;
+                        }else{//subtract
+                            colPos--;
+                        }
+                    }else{//change rows   
+                        if(direction > .5){//add
+                            rowPos++;
+                        }else{//subtract
+                            rowPos--;
+                        }
+                    }
+                }
+                if(board[rowPos][colPos] != ' '){
+                    if(orientation>.5){
+                        for(int j=0;j<9;j++){
+                            if(board[rowPos][j] == letter){
+                                board[rowPos][j] = ' ';
+                            }
+                        }
+                    }else{
+                        for(int k=0;k<9;k++){
+                            if(board[k][colPos] == letter){
+                                board[k][colPos] = ' ';
+                            }
+                        }
+                    }
+                    return false;
+                }
+                board[rowPos][colPos] = letter;
+
+               
+            }
+            return true;
+    }
+    public int playerGuess(int row, int col){
+
+	int totalShips = 12;
+
+	if (board[row][col] == 'a' || board[row][col] == 'b' || board[row][col] == 'c')
 	{
 		totalShips--;
 		if (totalShips == 0)
 		{
+           
 			return 3;
 		}
 		else 
 		{
-			//replace that part of computer board with a zero
-			board[row][col] = 1;
+			board[row][col] = ' ';
+         
 			return 1;
-			
 		}
-		
-		
 	}
 	else
 	{
-		board[row][col] = 0;
+		board[row][col] = ' ';
+     
 		return 0;
-		
 	}
 	
 	
 }
-	
+}
